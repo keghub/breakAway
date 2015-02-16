4d073e2341065a370532c3699203b4029e6f1239
Kevin Seger <kevin.seger@studentum.se>
2015-02-16 18:18:08 +0100

    Refactored retrieval of Destinations, Activities and CustomerTypes.

--- Pure/Web/Controllers/CustomerController.cs
+++ Pure/Web/Controllers/CustomerController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using BreakAway.Entities;
 using BreakAway.Models.Customer;
@@ -28,7 +28,9 @@ namespace BreakAway.Controllers
             {
                 ViewBag.message = message;
             }
-            IndexViewModel viewModel = new IndexViewModel();
+
+            var viewModel = new IndexViewModel();
+
             viewModel.Customers = (from customer in _repository.Customers
                                    select new CustomerItem
                                        {
@@ -51,40 +53,57 @@ namespace BreakAway.Controllers
 
             var customer = _repository.Customers.FirstOrDefault(p => p.Id == id);
 
-            var activities = _repository.Activities.AsEnumerable();
-            var destinations = _repository.Destinations.AsEnumerable();
+            if (customer == null)
+            {
+                return RedirectToAction("Index", "Customer");
+            }
 
-            EditViewModel viewModel = new EditViewModel
+            var viewModel = new EditViewModel
             {
                 Id = customer.Id,
                 FirstName = customer.FirstName,
                 LastName = customer.LastName,
                 Title = customer.Title,
                 CustomerTypeId = (int)customer.CustomerType,
-                CustomerTypes = from type in Enum.GetValues(typeof(CustomerType)).OfType<Enum>()
-                                select new SelectListItem
-                                {
-                                    Value = ((int)((CustomerType)type)).ToString(),
-                                    Text = type.ToString()
-                                },
+                CustomerTypes = GetCustomerTypes(),
                 Notes = customer.Notes,
                 PrimaryActivityId = customer.PrimaryActivityId.Value,
                 PrimaryDestinationId = customer.PrimaryDestinationId.Value,
-                Activities = from activity in activities
-                             select new SelectListItem
-                             {
-                                 Value = activity.Id.ToString(),
-                                 Text = activity.Name
-                             },
-                Destinations = from destination in destinations
-                               select new SelectListItem
-                               {
-                                   Value = destination.Id.ToString(),
-                                   Text = destination.Name
-                               }
+                Activities = GetActivities(),
+                Destinations = GetDestinations()
             };
 
             return View(viewModel);
+        }
+
+        private static IEnumerable<SelectListItem> GetCustomerTypes()
+        {
+            return from type in Enum.GetValues(typeof(CustomerType)).OfType<Enum>()
+                select new SelectListItem
+                {
+                    Value = ((int)((CustomerType)type)).ToString(CultureInfo.InvariantCulture),
+                    Text = type.ToString()
+                };
+        }
+
+        private IEnumerable<SelectListItem> GetActivities()
+        {
+            return from activity in _repository.Activities.AsEnumerable()
+                select new SelectListItem
+                {
+                    Value = activity.Id.ToString(CultureInfo.InvariantCulture),
+                    Text = activity.Name
+                };
+        }
+
+        private IEnumerable<SelectListItem> GetDestinations()
+        {
+            return from destination in _repository.Destinations.AsEnumerable()
+                select new SelectListItem
+                {
+                    Value = destination.Id.ToString(CultureInfo.InvariantCulture),
+                    Text = destination.Name
+                };
         }
 
         [HttpPost]
@@ -95,51 +114,34 @@ namespace BreakAway.Controllers
             {
                 return RedirectToAction("Edit","Customer", model);
             }
-            else
-            {
-                var customer = _repository.Customers.FirstOrDefault(p => p.Id == model.Id);
-                if (customer == null)
-                {
-                    return RedirectToAction("Index", "Customer", new { message = "Customer with id '" + model.Id + "' was not found" });
-                }
-                customer.FirstName = model.FirstName;
-                customer.LastName = model.LastName;
-                customer.Title = model.Title;
-                customer.CustomerType = (CustomerType)model.CustomerTypeId;
-                customer.Notes = model.Notes;
-                customer.PrimaryActivity = _repository.Activities.FirstOrDefault(p => p.Id == model.PrimaryActivityId);
-                customer.PrimaryDestination = _repository.Destinations.FirstOrDefault(p => p.Id == model.PrimaryDestinationId);
 
-                _repository.Save();
-                return RedirectToAction("Edit","Customer", new { id = customer.Id,message = "Changes saved successfully" });
+            var customer = _repository.Customers.FirstOrDefault(p => p.Id == model.Id);
+
+            if (customer == null)
+            {
+                return RedirectToAction("Index", "Customer", new { message = "Customer with id '" + model.Id + "' was not found" });
             }
+
+            customer.FirstName = model.FirstName;
+            customer.LastName = model.LastName;
+            customer.Title = model.Title;
+            customer.CustomerType = (CustomerType)model.CustomerTypeId;
+            customer.Notes = model.Notes;
+            customer.PrimaryActivity = _repository.Activities.FirstOrDefault(p => p.Id == model.PrimaryActivityId);
+            customer.PrimaryDestination = _repository.Destinations.FirstOrDefault(p => p.Id == model.PrimaryDestinationId);
+
+            _repository.Save();
+
+            return RedirectToAction("Edit","Customer", new { id = customer.Id,message = "Changes saved successfully" });
         }
 
         public ActionResult Add()
         {
-            var activities = _repository.Activities.AsEnumerable();
-            var destinations = _repository.Destinations.AsEnumerable();
-
-            AddViewModel viewModel = new AddViewModel
+            var viewModel = new AddViewModel
             {
-                CustomerTypes = from type in Enum.GetValues(typeof(CustomerType)).OfType<Enum>()
-                                select new SelectListItem
-                                {
-                                    Value = ((int)((CustomerType)type)).ToString(),
-                                    Text = type.ToString()
-                                },
-                Activities = from activity in activities
-                             select new SelectListItem
-                             {
-                                 Value = activity.Id.ToString(),
-                                 Text = activity.Name
-                             },
-                Destinations = from destination in destinations
-                               select new SelectListItem
-                               {
-                                   Value = destination.Id.ToString(),
-                                   Text = destination.Name
-                               }
+                CustomerTypes = GetCustomerTypes(),
+                Activities = GetActivities(),
+                Destinations = GetDestinations()
             };
 
             return View(viewModel);
@@ -153,24 +155,22 @@ namespace BreakAway.Controllers
             {
                 return RedirectToAction("Add", "Customer", new { message = "Customer not created" });
             }
-            else
-            {
-                Customer customer = new Customer
-                {
-                    FirstName = model.FirstName,
-                    LastName = model.LastName,
-                    Title = model.Title,
-                    CustomerType = (CustomerType)model.CustomerTypeId,
-                    Notes = model.Notes,
-                    PrimaryActivity = _repository.Activities.FirstOrDefault(p => p.Id == model.PrimaryActivityId),
-                    PrimaryDestination = _repository.Destinations.FirstOrDefault(p => p.Id == model.PrimaryDestinationId),
-                    ModifiedDate = DateTime.Now,
-                    AddDate = DateTime.Now
-                };
 
-                _repository.Customers.Add(customer);
-                _repository.Save();
-            }
+            var customer = new Customer
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Title = model.Title,
+                CustomerType = (CustomerType)model.CustomerTypeId,
+                Notes = model.Notes,
+                PrimaryActivity = _repository.Activities.FirstOrDefault(p => p.Id == model.PrimaryActivityId),
+                PrimaryDestination = _repository.Destinations.FirstOrDefault(p => p.Id == model.PrimaryDestinationId),
+                ModifiedDate = DateTime.Now,
+                AddDate = DateTime.Now
+            };
+
+            _repository.Customers.Add(customer);
+            _repository.Save();
 
             return RedirectToAction("index", "Customer", new { message = "Customer added successfully" });
         }
