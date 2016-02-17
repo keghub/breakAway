65c8fe3ac13755644a729565e1a5b67c0b3ee0b9
Kevin Seger <kevin.seger@studentum.se>
2016-02-17 14:16:33 +0100

    Breakaway MTBlanc Domain
    Added Domain.Cache to tracking

---
+++ MtBlanc/Domain/Domain.Cache/DomainCacheLoaderPackage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BreakAway
+{
+    public class DomainCacheLoaderPackage : Studentum.Infrastructure.Caching.ReflectionCacheLoaderPackage
+    {
+        public override System.Reflection.Assembly GetAssembly()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly();
+        }
+    }
+}
