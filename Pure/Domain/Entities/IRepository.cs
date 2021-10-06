d0630f16b8214c324a0a65cc88f7249a272688e6
Ethan Morgan <33624526+Ethanm12@users.noreply.github.com>
2021-10-06 16:26:47 +0200

    Convert IQueryable to ITable with static method. (#15)
    Added IQueryable to ITable extension method for Moq to accept AutoFixture generated DB Entities.
    
    (cherry picked from commit ccc4a2ab1e096ea0b7bc84ac95b55019d0a12d62)
    
    Co-authored-by: ethan.morgan <ethan.morgan@educations.com>

--- Pure/Domain/Entities/IRepository.cs
+++ Pure/Domain/Entities/IRepository.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace BreakAway.Entities
 {
@@ -15,12 +19,70 @@ namespace BreakAway.Entities
         ITable<Event> Events { get; }
 
         void Save();
-    }
+    } 
 
     public interface ITable<T> : IQueryable<T> where T : class
     {
         void Add(T item);
 
         void Delete(T item);
+    }
+
+    public static class QueryableExtension
+    {
+        public static ITable<T> AsTable<T>(this IQueryable<T> source) where T : class
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (source is ITable<T>)
+            {
+                return (ITable<T>)source;
+            }
+
+            return new EnumerableTable<T>(source);
+        }
+    }
+
+    internal class EnumerableTable<T> : ITable<T> where T : class
+    {
+        private IQueryable<T> _source;
+
+        public EnumerableTable(IQueryable<T> source)
+        {
+            _source = source;
+        }
+
+        public Expression Expression => _source.Expression;
+
+        public Type ElementType => _source.ElementType;
+
+        public IQueryProvider Provider => _source.Provider;
+
+        public void Add(T item)
+        {
+            var list = _source.ToList();
+            list.Add(item);
+            _source = list.AsQueryable();
+        }
+
+        public void Delete(T item)
+        {
+            var list = _source.ToList();
+            list.Remove(item);
+            _source = list.AsQueryable();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _source.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _source.GetEnumerator();
+        }
     }
 }
