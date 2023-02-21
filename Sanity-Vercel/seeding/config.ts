8653cba9b28e622890a4d112793ea6d08ed2c8e9
Henrik Agner <henrik.agner@educations.com>
2023-02-21 15:37:40 +0100

    Removed config variables

--- Sanity-Vercel/seeding/config.ts
+++ Sanity-Vercel/seeding/config.ts
@@ -1,7 +1,7 @@
 export const config = {
-    projectId: "p4e7l5rx",
-    dataset: "production",
+    projectId: "your_project_id",
+    dataset: "your_dataset",
     useCdn: false,
     apiVersion: '2021-10-21',
-    token: "skXLPskU53NhxmvzVcNbCF4Wpl2vOz5o0JI9eLuznTqaWLEpDiB4imjKKpAfExdb9MZkEy9nOKzxY4C9jAHaqoHWLo4OcMh2egbBpPIEJCW5TAUjKUp4a67FLNaSYlR2cXYyKIU9NqjNdmBmdkBsONS4ZSMhdziLMj5f9XPKCRKiyymJ1xe9",
+    token: "your_token",
 };
